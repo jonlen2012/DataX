c32b48f1f4596482054ca59b234961ec7e5b9366
静行 <xiaoyong.dengxy@alibaba-inc.com>
2014-09-23 14:36:21 +0800

    modify for ut

--- core/src/test/java/com/alibaba/datax/core/scheduler/standalone/StandAloneTestSlaveContainer.java
+++ core/src/test/java/com/alibaba/datax/core/scheduler/standalone/StandAloneTestSlaveContainer.java
@@ -1,12 +1,12 @@
 package com.alibaba.datax.core.scheduler.standalone;
 
 import com.alibaba.datax.common.util.Configuration;
-import com.alibaba.datax.core.container.AbstractContainer;
+import com.alibaba.datax.core.container.SlaveContainer;
 
 /**
  * Created by jingxing on 14-9-4.
  */
-public class StandAloneTestSlaveContainer extends AbstractContainer {
+public class StandAloneTestSlaveContainer extends SlaveContainer {
     public StandAloneTestSlaveContainer(Configuration configuration) {
         super(configuration);
     }
