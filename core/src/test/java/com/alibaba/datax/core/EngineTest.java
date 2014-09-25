c7a0a718048008461150602f4185f962e9fb506e
bazhen.csy <bazhen.csy@taobao.com>
2014-09-25 13:40:46 +0800

    增加Engine测试

---
+++ core/src/test/java/com/alibaba/datax/core/EngineTest.java
@@ -0,0 +1,50 @@
+package com.alibaba.datax.core;
+
+import java.io.File;
+import java.io.FileWriter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.alibaba.datax.common.util.Configuration;
+import com.alibaba.datax.core.Engine;
+import com.alibaba.datax.core.container.util.LoadUtil;
+import com.alibaba.datax.core.faker.FakeMasterContainer;
+import com.alibaba.datax.core.scaffold.base.CaseInitializer;
+import com.alibaba.datax.core.util.ConfigParser;
+import com.alibaba.datax.core.util.CoreConstant;
+
+/**
+ * Created by jingxing on 14-9-25.
+ */
+public class EngineTest extends CaseInitializer {
+	private Configuration configuration;
+
+	@Before
+	public void setUp() {
+		String path = EngineTest.class.getClassLoader().getResource(".")
+				.getFile();
+
+		this.configuration = ConfigParser.parse(path + File.separator
+				+ "all.json");
+		LoadUtil.bind(this.configuration);
+	}
+
+	@Test
+	public void test_entry() throws Throwable {
+		this.configuration.set(CoreConstant.DATAX_CORE_CONTAINER_MASTER_CLASS,
+				FakeMasterContainer.class.getName());
+
+		String jobConfig = this.configuration.toString();
+
+		String jobFile = "./job.json";
+		FileWriter writer = new FileWriter(jobFile);
+		writer.write(jobConfig);
+		writer.flush();
+		writer.close();
+
+		String[] args = { "-job", jobFile };
+		Engine.entry(args);
+	}
+
+}
