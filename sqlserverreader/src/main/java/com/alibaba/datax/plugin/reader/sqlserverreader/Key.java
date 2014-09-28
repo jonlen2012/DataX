29fdb34e1c17e515ed86336a856b689b4a5fdd8b
haiwei.luo <haiwei.luo@alibaba-inc.com>
2014-09-28 20:54:28 +0800

    commit code for sqlserverreader plugin

--- sqlserverreader/src/main/java/com/alibaba/datax/plugin/reader/sqlserverreader/Key.java
+++ sqlserverreader/src/main/java/com/alibaba/datax/plugin/reader/sqlserverreader/Key.java
@@ -4,26 +4,30 @@ package com.alibaba.datax.plugin.reader.sqlserverreader;
  * Created by haiwei.luo on 14-9-17.
  */
 public class Key {
-	public static final String DATABASE = "database";
-	
-	public static final String IP = "ip";
-	
-	public static final String PORT = "port";
-	
-	public static final String USER_NAME = "username";
-	
-	public static final String PASSWORD = "password";
-	
-	public static final String TABLE = "table";
-	
-	public static final String COLUMN = "column";
-	
-	public static final String WHERE = "where";
-	
-	public static final String SQL = "sql";
-	
-	public static final String ENCODING = "encoding";
-	
-	public static final String CONCURRENCY = "concurrency";
 
+	// must
+	public static final String JDBC_URL = "jdbcUrl";
+
+	// must
+	public static final String USERNAME = "username";
+
+	// must
+	public static final String PASSWORD = "password";
+
+	// must
+	public static final String TABLE = "table";
+
+	// not must, it's a List, default all columns
+	public static final String COLUMN = "column";
+
+	// not must, default nothing
+	public static final String SPLIT_PK = "splitPK";
+
+	// not must, default nothing
+	public static final String WHERE = "where";
+
+	// not must, default nothing
+	public static final String QUERYSQL = "querySql";
+	
+	public static final String FETCH_SIZE = "fetchSize";
 }
--- sqlserverreader/src/main/java/com/alibaba/datax/plugin/reader/sqlserverreader/SqlServerReader.java
+++ sqlserverreader/src/main/java/com/alibaba/datax/plugin/reader/sqlserverreader/SqlServerReader.java
@@ -1,8 +1,238 @@
 package com.alibaba.datax.plugin.reader.sqlserverreader;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.datax.common.element.BoolColumn;
+import com.alibaba.datax.common.element.BytesColumn;
+import com.alibaba.datax.common.element.DateColumn;
+import com.alibaba.datax.common.element.NumberColumn;
+import com.alibaba.datax.common.element.Record;
+import com.alibaba.datax.common.element.StringColumn;
+import com.alibaba.datax.common.exception.DataXException;
+import com.alibaba.datax.common.plugin.RecordSender;
+import com.alibaba.datax.common.spi.Reader;
+import com.alibaba.datax.common.util.Configuration;
+import com.alibaba.datax.plugin.rdbms.util.DBUtil;
+import com.alibaba.datax.plugin.rdbms.util.SqlFormatUtil;
+import com.alibaba.datax.plugin.reader.sqlserverreader.util.ConfigPretreatUtil;
+import com.alibaba.datax.plugin.reader.sqlserverreader.util.TableSplitUtil;
+
 /**
  * Created by haiwei.luo on 14-9-17.
  */
 public class SqlServerReader {
+	public static class Master extends Reader.Master {
+		private static final Logger LOG = LoggerFactory
+				.getLogger(SqlServerReader.Master.class);
+
+		private Configuration readerOriginConfig;
+
+		@Override
+		public void init() {
+			LOG.info("init() begin ...");
+			this.readerOriginConfig = this.getPluginJobConf();
+			ConfigPretreatUtil.validate(this.readerOriginConfig);
+			LOG.info("init() end ...");
+		}
+
+		@Override
+		public void prepare() {
+			LOG.info("prepare()");
+		}
+
+		@Override
+		public void post() {
+			LOG.info("post()");
+		}
+
+		@Override
+		public void destroy() {
+			LOG.info("destroy()");
+		}
+
+		@Override
+		public List<Configuration> split(int adviceNumber) {
+			LOG.info("split()");
+			return TableSplitUtil
+					.doSplit(this.readerOriginConfig, adviceNumber);
+		}
+
+	}
+
+	public static class Slave extends Reader.Slave {
+		private static final Logger LOG = LoggerFactory
+				.getLogger(SqlServerReader.Slave.class);
+
+		private Configuration readerSliceConfig;
+
+		private String jdbcUrl;
+		private String username;
+		private String password;
+		private int fetchSize;
+
+		private String _basicMessage;
+
+		@Override
+		public void init() {
+			LOG.info("init()");
+			this.readerSliceConfig = this.getPluginJobConf();
+
+			this.username = this.readerSliceConfig.getString(Key.USERNAME);
+			this.password = this.readerSliceConfig.getString(Key.PASSWORD);
+			this.jdbcUrl = this.readerSliceConfig.getString(Key.JDBC_URL);
+
+			this.fetchSize = this.readerSliceConfig.getInt(Key.FETCH_SIZE);
+
+			this._basicMessage = String.format("jdbcUrl: [%s]", this.jdbcUrl);
+		}
+
+		@Override
+		public void prepare() {
+			LOG.info("prepare()");
+		}
+
+		@Override
+		public void post() {
+			LOG.info("post()");
+		}
+
+		@Override
+		public void destroy() {
+			LOG.info("destroy()");
+		}
+
+		@Override
+		public void startRead(RecordSender recordSender) {
+			String sql = this.readerSliceConfig.getString(Key.QUERYSQL);
+
+			String formatedSql = null;
+
+			try {
+				formatedSql = SqlFormatUtil.format(sql);
+			} catch (Exception unused) {
+				// ignore it
+			}
+
+			LOG.info("do query [\n{}\n], from jdbc-url:[\n{}\n]",
+					null != formatedSql ? formatedSql : sql, this.jdbcUrl);
+
+			ResultSet rs = null;
+			Connection conn = DBUtil.getConnection("sqlserver", this.jdbcUrl,
+					this.username, this.password);
+
+			try {
+				rs = DBUtil.query(conn, sql, this.fetchSize);
+
+				while (rs.next()) {
+					this.transportOneRecord(recordSender, rs);
+				}
+			} catch (SQLException e) {
+				throw new DataXException(
+						SqlServerReaderErrorCode.RUNTIME_EXCEPTION, e);
+			} finally {
+				DBUtil.closeDBResources(rs, null, conn);
+			}
+
+		}
+
+		private void transportOneRecord(RecordSender recordSender, ResultSet rs) {
+			ResultSetMetaData metaData;
+			int columnNumber;
+			Record record = recordSender.createRecord();
+			try {
+				metaData = rs.getMetaData();
+				columnNumber = metaData.getColumnCount();
+
+				// warn:begin from 1
+				for (int i = 1; i < columnNumber; i++) {
+					switch (metaData.getColumnType(i)) {
+
+					case Types.CHAR:
+					case Types.NCHAR:
+					case Types.CLOB:
+					case Types.NCLOB:
+					case Types.VARCHAR:
+					case Types.LONGVARCHAR:
+					case Types.NVARCHAR:
+					case Types.LONGNVARCHAR:
+						record.addColumn(new StringColumn(rs.getString(i)));
+						break;
+
+					case Types.TINYINT:
+					case Types.SMALLINT:
+					case Types.INTEGER:
+						record.addColumn(new NumberColumn(rs.getInt(i)));
+						break;
+
+					case Types.BIGINT:
+					case Types.NUMERIC:
+						record.addColumn(new NumberColumn(rs.getLong(i)));
+						break;
+
+					case Types.DECIMAL:
+					case Types.FLOAT:
+					case Types.REAL:
+					case Types.DOUBLE:
+						record.addColumn(new NumberColumn(rs.getDouble(i)));
+						break;
+
+					case Types.DATE:
+						record.addColumn(new DateColumn(rs.getDate(i)));
+						break;
+					case Types.TIMESTAMP:
+						record.addColumn(new DateColumn(rs.getTimestamp(i)));
+						break;
+					case Types.TIME:
+						record.addColumn(new DateColumn(rs.getTime(i)));
+						break;
+
+					case Types.BINARY:
+					case Types.VARBINARY:
+					case Types.LONGVARBINARY:
+					case Types.BLOB:
+						record.addColumn(new BytesColumn(rs.getBytes(i)));
+						break;
+
+					// warn:BIT
+					case Types.BIT:
+					case Types.BOOLEAN:
+						record.addColumn(new BoolColumn(rs.getBoolean(i)));
+						break;
+
+					default:
+						throw new Exception(
+								String.format(
+										"unsupport SqlServer Data Type. ColumnName:[%s], ColumnType:[%s], ColumnClassName:[%s].",
+										metaData.getColumnName(i),
+										metaData.getColumnType(i),
+										metaData.getColumnClassName(i)));
+					}
+				}
+
+				recordSender.sendToWriter(record);
+
+			} catch (SQLException e) {
+				// 此异常不是脏数据
+				throw new DataXException(
+						SqlServerReaderErrorCode.RUNTIME_EXCEPTION,
+						"unable to get meta data.");
+			} catch (Exception e) {
+				// TODO 脏数据处理
+				this.getSlavePluginCollector().collectDirtyRecord(record,
+						e.getMessage());
+				// throw new DataXException(
+				// SqlServerReaderErrorCode.RUNTIME_EXCEPTION,
+				// e.getMessage(), e);
+			}
+		}
+	}
 
 }
