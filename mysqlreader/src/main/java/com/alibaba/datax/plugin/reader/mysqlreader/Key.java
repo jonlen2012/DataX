2630d7472f48d3bfed8354ab0b031e91b228a6e6
chinan.hjc <chinan.hjc@taobao.com>
2014-10-14 17:43:07 +0800

    add some mysqlreader sample.json

--- mysqlreader/src/main/java/com/alibaba/datax/plugin/reader/mysqlreader/Key.java
+++ mysqlreader/src/main/java/com/alibaba/datax/plugin/reader/mysqlreader/Key.java
@@ -1,22 +1,25 @@
 package com.alibaba.datax.plugin.reader.mysqlreader;
 
+/**
+ * 编码，时区等配置，暂未定.
+ */
 public final class Key {
-	public final static String JDBC_URL = "jdbcUrl";
+    public final static String JDBC_URL = "jdbcUrl";
 
-	public final static String USERNAME = "username";
+    public final static String USERNAME = "username";
 
-	public final static String PASSWORD = "password";
+    public final static String PASSWORD = "password";
 
-	public final static String TABLE = "table";
+    public final static String TABLE = "table";
 
-	// 是数组配置
-	public final static String COLUMN = "column";
+    // 是数组配置
+    public final static String COLUMN = "column";
 
-	public final static String QUERY_SQL = "querySql";
+    public final static String WHERE = "where";
 
-	public final static String WHERE = "where";
+    public final static String SPLIT_PK = "splitPk";
 
-	public final static String SESSION = "session";
+    public final static String QUERY_SQL = "querySql";
 
-	public final static String SPLIT_PK = "splitPk";
+    public final static String PRE_CHECK_SQL = "preCheckSql";
 }
