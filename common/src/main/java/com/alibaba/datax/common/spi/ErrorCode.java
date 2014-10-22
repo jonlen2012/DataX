26593ece8096c12fa9a3af2fcef4e2dd34fc78c1
bazhen.csy <bazhen.csy@taobao.com>
2014-10-22 21:50:39 +0800

    注释

--- common/src/main/java/com/alibaba/datax/common/spi/ErrorCode.java
+++ common/src/main/java/com/alibaba/datax/common/spi/ErrorCode.java
@@ -18,4 +18,16 @@ public interface ErrorCode {
 
 	// 错误码描述
 	String getDescription();
+	
+	/** 必须提供toString的实现
+	 * 
+	 * <pre>
+	 * &#064;Override
+	 * public String toString() {
+	 * 	return String.format(&quot;Code:[%s], Description:[%s]. &quot;, this.code, this.describe);
+	 * }
+	 * </pre>
+	 * 
+	 */
+	String toString();
 }
