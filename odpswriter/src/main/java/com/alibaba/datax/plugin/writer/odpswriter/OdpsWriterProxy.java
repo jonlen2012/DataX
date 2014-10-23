97eb9859203be7d5d53aa23b00af5e3292f17154
bazhen.csy <bazhen.csy@taobao.com>
2014-10-23 21:35:20 +0800

    格式化代码

--- odpswriter/src/main/java/com/alibaba/datax/plugin/writer/odpswriter/OdpsWriterProxy.java
+++ odpswriter/src/main/java/com/alibaba/datax/plugin/writer/odpswriter/OdpsWriterProxy.java
@@ -17,133 +17,159 @@ import java.io.IOException;
 import java.util.List;
 
 public class OdpsWriterProxy {
-    private static final Logger LOG = LoggerFactory.getLogger(OdpsWriterProxy.class);
+	private static final Logger LOG = LoggerFactory
+			.getLogger(OdpsWriterProxy.class);
 
-    private volatile boolean printColumnLess = true;// 是否打印对于源头字段数小于odps目的表的行的日志
+	private volatile boolean printColumnLess = true;// 是否打印对于源头字段数小于odps目的表的行的日志
 
-    private SlavePluginCollector slavePluginCollector;
+	private SlavePluginCollector slavePluginCollector;
 
+	private Upload slaveUpload;
 
-    private Upload slaveUpload;
-    private com.alibaba.datax.common.element.Record dataxRecord;
-    private RecordSchema schema;
-    private ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
-            70 * 1024 * 1024);
-    private int max_buffer_length = 64 * 1024 * 1024;
-    private ProtobufRecordWriter protobufRecordWriter = null;
-    private long blockId;
-    private int intervalStep;
-    private List<Integer> columnPositions;
-    private List<Column.Type> tableOriginalColumnTypeList;
+	private com.alibaba.datax.common.element.Record dataxRecord;
 
-    public OdpsWriterProxy(Upload slaveUpload, long blockId, int intervalStep,
-                           List<Integer> columnPositions, SlavePluginCollector slavePluginCollector)
-            throws IOException {
-        this.slaveUpload = slaveUpload;
-        this.schema = this.slaveUpload.getSchema();
-        this.tableOriginalColumnTypeList = OdpsUtil
-                .getTableOriginalColumnTypeList(this.schema);
+	private RecordSchema schema;
 
-        this.protobufRecordWriter = new ProtobufRecordWriter(schema, byteArrayOutputStream);
-        this.blockId = blockId;
-        this.intervalStep = intervalStep;
-        this.columnPositions = columnPositions;
-        this.slavePluginCollector = slavePluginCollector;
+	private ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
+			70 * 1024 * 1024);
 
-    }
+	private int max_buffer_length = 64 * 1024 * 1024;
 
-    public void writeOneRecord(com.alibaba.datax.common.element.Record dataXRecord, List<Long> blocks)
-            throws Exception {
+	private ProtobufRecordWriter protobufRecordWriter = null;
 
-        Record record = dataxRecordToOdpsRecord(dataXRecord, schema);
+	private long blockId;
 
-        if (null != record) {
-            protobufRecordWriter.write(record);
-        }
+	private int intervalStep;
 
-        if (byteArrayOutputStream.size() >= max_buffer_length) {
-            protobufRecordWriter.close();
-            OdpsUtil.slaveWriteOneBlock(this.slaveUpload, this.byteArrayOutputStream, blockId);
-            LOG.info("write block {} ok.", blockId);
+	private List<Integer> columnPositions;
 
-            blocks.add(blockId);
-            byteArrayOutputStream.reset();
-            protobufRecordWriter = new ProtobufRecordWriter(schema, byteArrayOutputStream);
+	private List<Column.Type> tableOriginalColumnTypeList;
 
-            blockId += this.intervalStep;
-        }
-    }
+	public OdpsWriterProxy(Upload slaveUpload, long blockId, int intervalStep,
+			List<Integer> columnPositions,
+			SlavePluginCollector slavePluginCollector) throws IOException {
+		this.slaveUpload = slaveUpload;
+		this.schema = this.slaveUpload.getSchema();
+		this.tableOriginalColumnTypeList = OdpsUtil
+				.getTableOriginalColumnTypeList(this.schema);
 
-    public void writeRemainingRecord(List<Long> blocks) throws Exception {
-        // complete protobuf stream, then write to http
-        protobufRecordWriter.close();
-        if (byteArrayOutputStream.size() != 0) {
-            OdpsUtil.slaveWriteOneBlock(this.slaveUpload, this.byteArrayOutputStream, blockId);
-            LOG.info("write block {} ok.", blockId);
+		this.protobufRecordWriter = new ProtobufRecordWriter(schema,
+				byteArrayOutputStream);
+		this.blockId = blockId;
+		this.intervalStep = intervalStep;
+		this.columnPositions = columnPositions;
+		this.slavePluginCollector = slavePluginCollector;
 
-            blocks.add(blockId);
-            // reset the buffer for next block
-            byteArrayOutputStream.reset();
-        }
-    }
+	}
 
-    public Record dataxRecordToOdpsRecord(com.alibaba.datax.common.element.Record dataXRecord,
-                                          RecordSchema schema)
-            throws Exception {
-        int sourceColumnCount = dataXRecord.getColumnNumber();
-        int destColumnCount = schema.getColumnCount();
-        Record odpsRecord = new Record(destColumnCount);
+	public void writeOneRecord(
+			com.alibaba.datax.common.element.Record dataXRecord,
+			List<Long> blocks) throws Exception {
 
-        int userConfiguredColumnNumber = this.columnPositions.size();
+		Record record = dataxRecordToOdpsRecord(dataXRecord, schema);
 
-        if (sourceColumnCount > userConfiguredColumnNumber) {
-            String businessMessage = String.format("source columnNumber=[%s] bigger than configured destination columnNumber=[%s].",
-                    sourceColumnCount, userConfiguredColumnNumber);
-            String message = StrUtil.buildOriginalCauseMessage(
-                    businessMessage, null);
-            LOG.error(message);
+		if (null != record) {
+			protobufRecordWriter.write(record);
+		}
 
-            throw new DataXException(OdpsWriterErrorCode.COLUMN_NUMBER_ERROR, businessMessage);
-        } else if (sourceColumnCount < userConfiguredColumnNumber) {
-            if (printColumnLess) {
-                printColumnLess = false;
-                LOG.warn("source columnNumber={} is less than configured destination columnNumber={}, DataX will fill some column with null.",
-                        dataXRecord.getColumnNumber(), userConfiguredColumnNumber);
-            }
-        }
+		if (byteArrayOutputStream.size() >= max_buffer_length) {
+			protobufRecordWriter.close();
+			OdpsUtil.slaveWriteOneBlock(this.slaveUpload,
+					this.byteArrayOutputStream, blockId);
+			LOG.info("write block {} ok.", blockId);
 
-        int currentIndex = -1;
-        int sourceIndex = 0;
-        try {
-            for (int len = sourceColumnCount; sourceIndex < len; sourceIndex++) {
-                currentIndex = columnPositions.get(sourceIndex);
-                Column.Type type = this.tableOriginalColumnTypeList.get(currentIndex);
-                switch (type) {
-                    case ODPS_STRING:
-                        odpsRecord.setString(currentIndex, dataXRecord.getColumn(sourceIndex).asString());
-                        break;
-                    case ODPS_BIGINT:
-                        odpsRecord.setBigint(currentIndex, dataXRecord.getColumn(sourceIndex).asLong());
-                        break;
-                    case ODPS_BOOLEAN:
-                        odpsRecord.setBoolean(currentIndex, dataXRecord.getColumn(sourceIndex).asBoolean());
-                        break;
-                    case ODPS_DATETIME:
-                        odpsRecord.setDatetime(currentIndex, dataXRecord.getColumn(sourceIndex).asDate());
-                        break;
-                    case ODPS_DOUBLE:
-                        odpsRecord.setDouble(currentIndex, dataXRecord.getColumn(sourceIndex).asDouble());
-                        break;
-                    default:
-                        break;
-                }
-            }
-        } catch (Exception e) {
-            String message = String.format("Dirty record detail:sourceIndex=[%s], value=[%s].", sourceIndex,
-                    dataxRecord.getColumn(sourceColumnCount));
-            this.slavePluginCollector.collectDirtyRecord(dataXRecord, e, message);
-        }
+			blocks.add(blockId);
+			byteArrayOutputStream.reset();
+			protobufRecordWriter = new ProtobufRecordWriter(schema,
+					byteArrayOutputStream);
 
-        return odpsRecord;
-    }
+			blockId += this.intervalStep;
+		}
+	}
+
+	public void writeRemainingRecord(List<Long> blocks) throws Exception {
+		// complete protobuf stream, then write to http
+		protobufRecordWriter.close();
+		if (byteArrayOutputStream.size() != 0) {
+			OdpsUtil.slaveWriteOneBlock(this.slaveUpload,
+					this.byteArrayOutputStream, blockId);
+			LOG.info("write block {} ok.", blockId);
+
+			blocks.add(blockId);
+			// reset the buffer for next block
+			byteArrayOutputStream.reset();
+		}
+	}
+
+	public Record dataxRecordToOdpsRecord(
+			com.alibaba.datax.common.element.Record dataXRecord,
+			RecordSchema schema) throws Exception {
+		int sourceColumnCount = dataXRecord.getColumnNumber();
+		int destColumnCount = schema.getColumnCount();
+		Record odpsRecord = new Record(destColumnCount);
+
+		int userConfiguredColumnNumber = this.columnPositions.size();
+
+		if (sourceColumnCount > userConfiguredColumnNumber) {
+			String businessMessage = String
+					.format("source columnNumber=[%s] bigger than configured destination columnNumber=[%s].",
+							sourceColumnCount, userConfiguredColumnNumber);
+			String message = StrUtil.buildOriginalCauseMessage(businessMessage,
+					null);
+			LOG.error(message);
+
+			throw new DataXException(OdpsWriterErrorCode.COLUMN_NUMBER_ERROR,
+					businessMessage);
+		} else if (sourceColumnCount < userConfiguredColumnNumber) {
+			if (printColumnLess) {
+				printColumnLess = false;
+				LOG.warn(
+						"source columnNumber={} is less than configured destination columnNumber={}, DataX will fill some column with null.",
+						dataXRecord.getColumnNumber(),
+						userConfiguredColumnNumber);
+			}
+		}
+
+		int currentIndex = -1;
+		int sourceIndex = 0;
+		try {
+			for (int len = sourceColumnCount; sourceIndex < len; sourceIndex++) {
+				currentIndex = columnPositions.get(sourceIndex);
+				Column.Type type = this.tableOriginalColumnTypeList
+						.get(currentIndex);
+				switch (type) {
+				case ODPS_STRING:
+					odpsRecord.setString(currentIndex,
+							dataXRecord.getColumn(sourceIndex).asString());
+					break;
+				case ODPS_BIGINT:
+					odpsRecord.setBigint(currentIndex,
+							dataXRecord.getColumn(sourceIndex).asLong());
+					break;
+				case ODPS_BOOLEAN:
+					odpsRecord.setBoolean(currentIndex,
+							dataXRecord.getColumn(sourceIndex).asBoolean());
+					break;
+				case ODPS_DATETIME:
+					odpsRecord.setDatetime(currentIndex,
+							dataXRecord.getColumn(sourceIndex).asDate());
+					break;
+				case ODPS_DOUBLE:
+					odpsRecord.setDouble(currentIndex,
+							dataXRecord.getColumn(sourceIndex).asDouble());
+					break;
+				default:
+					break;
+				}
+			}
+		} catch (Exception e) {
+			String message = String.format(
+					"Dirty record detail:sourceIndex=[%s], value=[%s].",
+					sourceIndex, dataxRecord.getColumn(sourceColumnCount));
+			this.slavePluginCollector.collectDirtyRecord(dataXRecord, e,
+					message);
+		}
+
+		return odpsRecord;
+	}
 }
--- streamreader/src/main/java/com/alibaba/datax/plugin/reader/streamreader/StreamReader.java
+++ streamreader/src/main/java/com/alibaba/datax/plugin/reader/streamreader/StreamReader.java
@@ -14,184 +14,202 @@ import java.util.List;
 
 public class StreamReader extends Reader {
 
-    public static class Master extends Reader.Master {
+	public static class Master extends Reader.Master {
 
-        private Configuration originalConfig;
+		private Configuration originalConfig;
 
-        @Override
-        public void init() {
-            this.originalConfig = super.getPluginJobConf();
-            dealColumn(this.originalConfig);
+		@Override
+		public void init() {
+			this.originalConfig = super.getPluginJobConf();
+			dealColumn(this.originalConfig);
 
-            Long sliceRecordCount = this.originalConfig.getLong(Key.SLICE_RECORD_COUNT);
-            if (null == sliceRecordCount) {
-                throw new DataXException(StreamReaderErrorCode.REQUIRED_VALUE, "Lost config sliceRecordCount.");
-            } else if (sliceRecordCount < 1) {
-                throw new DataXException(StreamReaderErrorCode.ILLEGAL_VALUE, "sliceRecordCount can not <1.");
-            }
+			Long sliceRecordCount = this.originalConfig
+					.getLong(Key.SLICE_RECORD_COUNT);
+			if (null == sliceRecordCount) {
+				throw new DataXException(StreamReaderErrorCode.REQUIRED_VALUE,
+						"Lost config sliceRecordCount.");
+			} else if (sliceRecordCount < 1) {
+				throw new DataXException(StreamReaderErrorCode.ILLEGAL_VALUE,
+						"sliceRecordCount can not <1.");
+			}
 
-        }
+		}
 
-        private void dealColumn(Configuration originalConfig) {
-            List<JSONObject> columns = originalConfig.getList(Key.COLUMN, JSONObject.class);
-            if (null == columns || columns.isEmpty()) {
-                throw new DataXException(StreamReaderErrorCode.REQUIRED_VALUE, "Lost config column.");
-            }
+		private void dealColumn(Configuration originalConfig) {
+			List<JSONObject> columns = originalConfig.getList(Key.COLUMN,
+					JSONObject.class);
+			if (null == columns || columns.isEmpty()) {
+				throw new DataXException(StreamReaderErrorCode.REQUIRED_VALUE,
+						"Lost config column.");
+			}
 
-            List<String> dealedColumns = new ArrayList<String>();
-            for (JSONObject eachColumn : columns) {
-                Configuration eachColumnConfig = Configuration.from(eachColumn);
-                eachColumnConfig.getNecessaryValue(Constant.VALUE, StreamReaderErrorCode.REQUIRED_VALUE);
-                String typeName = eachColumnConfig.getString(Constant.TYPE);
-                if (StringUtils.isBlank(typeName)) {
-                    // empty typeName will be set to default type: string
-                    eachColumnConfig.set(Constant.TYPE, Type.STRING);
-                } else {
-                    if (Type.DATE.name().equalsIgnoreCase(typeName)) {
-                        boolean notAssignDateFormat = StringUtils.isBlank(eachColumnConfig.getString(
-                                Constant.DATE_FORMAT_MARK));
-                        if (notAssignDateFormat) {
-                            eachColumnConfig.set(Constant.DATE_FORMAT_MARK, Constant.DEFAULT_DATE_FORMAT);
-                        }
-                    }
-                    if (!Type.isTypeIllegal(typeName)) {
-                        throw new DataXException(StreamReaderErrorCode.NOT_SUPPORT_TYPE,
-                                "Unsupported type:" + typeName);
-                    }
-                }
-                dealedColumns.add(eachColumnConfig.toJSON());
-            }
+			List<String> dealedColumns = new ArrayList<String>();
+			for (JSONObject eachColumn : columns) {
+				Configuration eachColumnConfig = Configuration.from(eachColumn);
+				eachColumnConfig.getNecessaryValue(Constant.VALUE,
+						StreamReaderErrorCode.REQUIRED_VALUE);
+				String typeName = eachColumnConfig.getString(Constant.TYPE);
+				if (StringUtils.isBlank(typeName)) {
+					// empty typeName will be set to default type: string
+					eachColumnConfig.set(Constant.TYPE, Type.STRING);
+				} else {
+					if (Type.DATE.name().equalsIgnoreCase(typeName)) {
+						boolean notAssignDateFormat = StringUtils
+								.isBlank(eachColumnConfig
+										.getString(Constant.DATE_FORMAT_MARK));
+						if (notAssignDateFormat) {
+							eachColumnConfig.set(Constant.DATE_FORMAT_MARK,
+									Constant.DEFAULT_DATE_FORMAT);
+						}
+					}
+					if (!Type.isTypeIllegal(typeName)) {
+						throw new DataXException(
+								StreamReaderErrorCode.NOT_SUPPORT_TYPE,
+								"Unsupported type:" + typeName);
+					}
+				}
+				dealedColumns.add(eachColumnConfig.toJSON());
+			}
 
-            originalConfig.set(Key.COLUMN, dealedColumns);
-        }
+			originalConfig.set(Key.COLUMN, dealedColumns);
+		}
 
-        @Override
-        public void prepare() {
-        }
+		@Override
+		public void prepare() {
+		}
 
-        @Override
-        public List<Configuration> split(int adviceNumber) {
-            List<Configuration> configurations = new ArrayList<Configuration>();
+		@Override
+		public List<Configuration> split(int adviceNumber) {
+			List<Configuration> configurations = new ArrayList<Configuration>();
 
-            for (int i = 0; i < adviceNumber; i++) {
-                configurations.add(this.originalConfig.clone());
-            }
-            return configurations;
-        }
+			for (int i = 0; i < adviceNumber; i++) {
+				configurations.add(this.originalConfig.clone());
+			}
+			return configurations;
+		}
 
-        @Override
-        public void post() {
-        }
+		@Override
+		public void post() {
+		}
 
-        @Override
-        public void destroy() {
-        }
+		@Override
+		public void destroy() {
+		}
 
-    }
+	}
 
-    public static class Slave extends Reader.Slave {
+	public static class Slave extends Reader.Slave {
 
-        private Configuration readerSliceConfig;
-        private List<String> columns;
-        private long sliceRecordCount;
+		private Configuration readerSliceConfig;
 
+		private List<String> columns;
 
-        @Override
-        public void init() {
-            this.readerSliceConfig = super.getPluginJobConf();
-            this.columns = this.readerSliceConfig.getList(Key.COLUMN, String.class);
+		private long sliceRecordCount;
 
-            this.sliceRecordCount = this.readerSliceConfig.getLong(Key.SLICE_RECORD_COUNT);
-        }
+		@Override
+		public void init() {
+			this.readerSliceConfig = super.getPluginJobConf();
+			this.columns = this.readerSliceConfig.getList(Key.COLUMN,
+					String.class);
 
-        @Override
-        public void prepare() {
-        }
+			this.sliceRecordCount = this.readerSliceConfig
+					.getLong(Key.SLICE_RECORD_COUNT);
+		}
 
-        @Override
-        public void startRead(RecordSender recordSender) {
-            Record oneRecord = buildOneRecord(recordSender, this.columns);
+		@Override
+		public void prepare() {
+		}
 
-            while (this.sliceRecordCount > 0) {
-                recordSender.sendToWriter(oneRecord);
-                this.sliceRecordCount--;
-            }
-        }
+		@Override
+		public void startRead(RecordSender recordSender) {
+			Record oneRecord = buildOneRecord(recordSender, this.columns);
 
-        @Override
-        public void post() {
-        }
+			while (this.sliceRecordCount > 0) {
+				recordSender.sendToWriter(oneRecord);
+				this.sliceRecordCount--;
+			}
+		}
 
-        @Override
-        public void destroy() {
-        }
+		@Override
+		public void post() {
+		}
 
-        private Record buildOneRecord(RecordSender recordSender, List<String> columns) {
-            if (null == recordSender) {
-                throw new IllegalArgumentException("Parameter recordSender can not be null.");
-            }
+		@Override
+		public void destroy() {
+		}
 
-            if (null == columns || columns.isEmpty()) {
-                throw new IllegalArgumentException("Parameter columns can not be null nor empty.");
-            }
+		private Record buildOneRecord(RecordSender recordSender,
+				List<String> columns) {
+			if (null == recordSender) {
+				throw new IllegalArgumentException(
+						"Parameter recordSender can not be null.");
+			}
 
-            Record record = recordSender.createRecord();
-            try {
-                for (String eachColumn : columns) {
-                    Configuration eachColumnConfig = Configuration.from(eachColumn);
-                    String columnValue = eachColumnConfig.getString(Constant.VALUE);
-                    Type columnType = Type.valueOf(eachColumnConfig.getString(Constant.TYPE).toUpperCase());
-                    switch (columnType) {
-                        case STRING:
-                            record.addColumn(new StringColumn(columnValue));
-                            break;
-                        case INT:
-                            record.addColumn(new LongColumn(columnValue));
-                            break;
-                        case DOUBLE:
-                            record.addColumn(new DoubleColumn(columnValue));
-                            break;
-                        case DATE:
-                            SimpleDateFormat format = new SimpleDateFormat(
-                                    eachColumnConfig.getString(Constant.DATE_FORMAT_MARK));
-                            record.addColumn(new DateColumn(format.parse(columnValue)));
-                            break;
-                        case BOOLEAN:
-                            record.addColumn(new BoolColumn("true".equalsIgnoreCase(columnValue) ? true : false));
-                            break;
-                        case BYTES:
-                            record.addColumn(new BytesColumn(columnValue.getBytes()));
-                            break;
-                        default:
-                            //in fact,never to be here
-                            throw new Exception("Unsupported type:" + columnType.name());
-                    }
-                }
-            } catch (Exception e) {
-                throw new DataXException(StreamReaderErrorCode.ILLEGAL_VALUE, "Construct one record failed.", e);
-            }
+			if (null == columns || columns.isEmpty()) {
+				throw new IllegalArgumentException(
+						"Parameter columns can not be null nor empty.");
+			}
 
-            return record;
-        }
-    }
+			Record record = recordSender.createRecord();
+			try {
+				for (String eachColumn : columns) {
+					Configuration eachColumnConfig = Configuration
+							.from(eachColumn);
+					String columnValue = eachColumnConfig
+							.getString(Constant.VALUE);
+					Type columnType = Type.valueOf(eachColumnConfig.getString(
+							Constant.TYPE).toUpperCase());
+					switch (columnType) {
+					case STRING:
+						record.addColumn(new StringColumn(columnValue));
+						break;
+					case LONG:
+						record.addColumn(new LongColumn(columnValue));
+						break;
+					case DOUBLE:
+						record.addColumn(new DoubleColumn(columnValue));
+						break;
+					case DATE:
+						SimpleDateFormat format = new SimpleDateFormat(
+								eachColumnConfig
+										.getString(Constant.DATE_FORMAT_MARK));
+						record.addColumn(new DateColumn(format
+								.parse(columnValue)));
+						break;
+					case BOOL:
+						record.addColumn(new BoolColumn("true"
+								.equalsIgnoreCase(columnValue) ? true : false));
+						break;
+					case BYTES:
+						record.addColumn(new BytesColumn(columnValue.getBytes()));
+						break;
+					default:
+						// in fact,never to be here
+						throw new Exception("Unsupported type:"
+								+ columnType.name());
+					}
+				}
+			} catch (Exception e) {
+				throw new DataXException(StreamReaderErrorCode.ILLEGAL_VALUE,
+						"Construct one record failed.", e);
+			}
 
-    private enum Type {
-        STRING,
-        INT,
-        BOOLEAN,
-        DOUBLE,
-        DATE,
-        BYTES,;
+			return record;
+		}
+	}
 
-        private static boolean isTypeIllegal(String typeString) {
-            try {
-                Type.valueOf(typeString.toUpperCase());
-            } catch (Exception e) {
-                return false;
-            }
+	private enum Type {
+		STRING, LONG, BOOL, DOUBLE, DATE, BYTES, ;
 
-            return true;
-        }
-    }
+		private static boolean isTypeIllegal(String typeString) {
+			try {
+				Type.valueOf(typeString.toUpperCase());
+			} catch (Exception e) {
+				return false;
+			}
+
+			return true;
+		}
+	}
 
 }
