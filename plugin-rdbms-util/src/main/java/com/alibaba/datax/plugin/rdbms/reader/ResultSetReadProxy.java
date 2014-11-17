e0f37d6d51df4c65d317a643f5bd1ef7ece91322
haiwei.luo <haiwei.luo@alibaba-inc.com>
2014-11-17 14:30:30 +0800

    mysql reader 对于year类型的处理，example：数据库year 2014 ，读出为2014，原来为2014-01-01（Date类型处理的），这个改动后，mysql reader/writer对于year类型，保持了一致

--- plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/reader/ResultSetReadProxy.java
+++ plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/reader/ResultSetReadProxy.java
@@ -13,97 +13,105 @@ import java.sql.ResultSetMetaData;
 import java.sql.Types;
 
 public class ResultSetReadProxy {
-    private static final Logger LOG = LoggerFactory
-            .getLogger(ResultSetReadProxy.class);
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ResultSetReadProxy.class);
 
-    private static final boolean IS_DEBUG = LOG.isDebugEnabled();
+	private static final boolean IS_DEBUG = LOG.isDebugEnabled();
 
-    public static void transportOneRecord(RecordSender recordSender, ResultSet rs,
-                                          ResultSetMetaData metaData, int columnNumber,
-                                          SlavePluginCollector slavePluginCollector) {
-        Record record = recordSender.createRecord();
+	public static void transportOneRecord(RecordSender recordSender,
+			ResultSet rs, ResultSetMetaData metaData, int columnNumber,
+			SlavePluginCollector slavePluginCollector) {
+		Record record = recordSender.createRecord();
 
-        try {
-            for (int i = 1; i <= columnNumber; i++) {
-                switch (metaData.getColumnType(i)) {
+		try {
+			for (int i = 1; i <= columnNumber; i++) {
+				switch (metaData.getColumnType(i)) {
 
-                    case Types.CHAR:
-                    case Types.NCHAR:
-                    case Types.CLOB:
-                    case Types.NCLOB:
-                    case Types.VARCHAR:
-                    case Types.LONGVARCHAR:
-                    case Types.NVARCHAR:
-                    case Types.LONGNVARCHAR:
-                        record.addColumn(new StringColumn(rs.getString(i)));
-                        break;
+				case Types.CHAR:
+				case Types.NCHAR:
+				case Types.CLOB:
+				case Types.NCLOB:
+				case Types.VARCHAR:
+				case Types.LONGVARCHAR:
+				case Types.NVARCHAR:
+				case Types.LONGNVARCHAR:
+					record.addColumn(new StringColumn(rs.getString(i)));
+					break;
 
-                    case Types.SMALLINT:
-                    case Types.TINYINT:
-                    case Types.INTEGER:
-                    case Types.BIGINT:
-                        record.addColumn(new LongColumn(rs.getString(i)));
-                        break;
+				case Types.SMALLINT:
+				case Types.TINYINT:
+				case Types.INTEGER:
+				case Types.BIGINT:
+					record.addColumn(new LongColumn(rs.getString(i)));
+					break;
 
-                    case Types.NUMERIC:
-                    case Types.DECIMAL:
-                        record.addColumn(new DoubleColumn(rs.getString(i)));
-                        break;
+				case Types.NUMERIC:
+				case Types.DECIMAL:
+					record.addColumn(new DoubleColumn(rs.getString(i)));
+					break;
 
-                    case Types.FLOAT:
-                    case Types.REAL:
-                    case Types.DOUBLE:
-                        record.addColumn(new DoubleColumn(rs.getString(i)));
-                        break;
+				case Types.FLOAT:
+				case Types.REAL:
+				case Types.DOUBLE:
+					record.addColumn(new DoubleColumn(rs.getString(i)));
+					break;
 
-                    case Types.TIME:
-                        record.addColumn(new DateColumn(rs.getTime(i)));
-                        break;
+				case Types.TIME:
+					record.addColumn(new DateColumn(rs.getTime(i)));
+					break;
 
-                    case Types.DATE:
-                        record.addColumn(new DateColumn(rs.getDate(i)));
-                        break;
+				// for mysql bug, see http://bugs.mysql.com/bug.php?id=35115
+				case Types.DATE:
+					if (metaData.getColumnTypeName(i).equalsIgnoreCase("year")) {
+						record.addColumn(new StringColumn(rs.getString(i)));
+					} else {
+						record.addColumn(new DateColumn(rs.getDate(i)));
+					}
+					break;
 
-                    case Types.TIMESTAMP:
-                        record.addColumn(new DateColumn(rs.getTimestamp(i)));
-                        break;
+				case Types.TIMESTAMP:
+					record.addColumn(new DateColumn(rs.getTimestamp(i)));
+					break;
 
-                    case Types.BINARY:
-                    case Types.VARBINARY:
-                    case Types.BLOB:
-                    case Types.LONGVARBINARY:
-                        record.addColumn(new BytesColumn(rs.getBytes(i)));
-                        break;
+				case Types.BINARY:
+				case Types.VARBINARY:
+				case Types.BLOB:
+				case Types.LONGVARBINARY:
+					record.addColumn(new BytesColumn(rs.getBytes(i)));
+					break;
 
-                    // TODO  可能要把 bit 删除
-                    case Types.BOOLEAN:
-                    case Types.BIT:
-                        record.addColumn(new BoolColumn(rs.getBoolean(i)));
-                        break;
+				// TODO  可能要把 bit 删除
+				case Types.BOOLEAN:
+				case Types.BIT:
+					record.addColumn(new BoolColumn(rs.getBoolean(i)));
+					break;
 
-                    case Types.NULL:
-                        record.addColumn(new NullColumn(rs.getObject(i)));
-                        break;
+				case Types.NULL:
+					record.addColumn(new NullColumn(rs.getObject(i)));
+					break;
 
-                    // TODO 添加BASIC_MESSAGE
-                    default:
-                        throw DataXException.asDataXException(DBUtilErrorCode.UNSUPPORTED_TYPE,
-                                String.format(
-                                        "DataX 不支持数据库读取这种字段类型. ColumnName:[%s], ColumnType:[%s], ColumnClassName:[%s].",
-                                        metaData.getColumnName(i),
-                                        metaData.getColumnType(i),
-                                        metaData.getColumnClassName(i)));
-                }
-            }
-            recordSender.sendToWriter(record);
-        } catch (Exception e) {
-            if (IS_DEBUG) {
-                LOG.debug("read data " + record.toString() + " occur exception:", e);
-            }
-            slavePluginCollector.collectDirtyRecord(record, e);
-            if (e instanceof DataXException) {
-                throw (DataXException) e;
-            }
-        }
-    }
+				// TODO 添加BASIC_MESSAGE
+				default:
+					throw DataXException
+							.asDataXException(
+									DBUtilErrorCode.UNSUPPORTED_TYPE,
+									String.format(
+											"DataX 不支持数据库读取这种字段类型. ColumnName:[%s], ColumnType:[%s], ColumnClassName:[%s].",
+											metaData.getColumnName(i),
+											metaData.getColumnType(i),
+											metaData.getColumnClassName(i)));
+				}
+			}
+			recordSender.sendToWriter(record);
+		} catch (Exception e) {
+			if (IS_DEBUG) {
+				LOG.debug("read data " + record.toString()
+						+ " occur exception:", e);
+			}
+			slavePluginCollector.collectDirtyRecord(record, e);
+			if (e instanceof DataXException) {
+				throw (DataXException) e;
+			}
+		}
+	}
 }
