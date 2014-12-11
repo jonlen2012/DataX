7aa8b55478308aa5f605a2ca5b9c50a8da03c221
chinan.hjc <chinan.hjc@taobao.com>
2014-12-11 16:55:19 +0800

    分布式 DataX

--- core/src/main/java/com/alibaba/datax/core/statistics/collector/container/distribute/DistributeTaskGroupContainerCollector.java
+++ core/src/main/java/com/alibaba/datax/core/statistics/collector/container/distribute/DistributeTaskGroupContainerCollector.java
@@ -3,6 +3,9 @@ package com.alibaba.datax.core.statistics.collector.container.distribute;
 import com.alibaba.datax.common.util.Configuration;
 import com.alibaba.datax.core.statistics.collector.container.AbstractTaskGroupContainerCollector;
 import com.alibaba.datax.core.statistics.communication.Communication;
+import com.alibaba.datax.core.statistics.communication.CommunicationManager;
+import com.alibaba.datax.core.util.DataxServiceUtil;
+import com.alibaba.datax.service.face.domain.TaskGroupStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,12 +21,18 @@ public class DistributeTaskGroupContainerCollector extends AbstractTaskGroupCont
 
     @Override
     public void report(Communication communication) {
-        /**
-         * URL：PUT /inner/job/{jobId}/taskGroup/{taskGroupId}/status
-         * TODO 汇报给 DataX service
-         *
-         *
-         */
+        TaskGroupStatus taskGroupStatus = new TaskGroupStatus();
+
+        taskGroupStatus.setState(communication.getState());
+        taskGroupStatus.setTotalRecords(communication.getLongCounter("totalReadRecords"));
+        taskGroupStatus.setTotalBytes(communication.getLongCounter("totalReadBytes"));
+        taskGroupStatus.setSpeedRecords(communication.getLongCounter(CommunicationManager.RECORD_SPEED));
+        taskGroupStatus.setSpeedBytes(communication.getLongCounter(CommunicationManager.BYTE_SPEED));
+        taskGroupStatus.setErrorRecords(CommunicationManager.getTotalErrorRecords(communication));
+        taskGroupStatus.setErrorBytes(CommunicationManager.getTotalErrorBytes(communication));
+        taskGroupStatus.setErrorMessage(communication.getThrowableMessage());
+
+        DataxServiceUtil.updateTaskGroupInfo(super.jobId, super.taskGroupId, taskGroupStatus);
     }
 
 }
