9918ccb571bdb696e21c9e250cff7c0126239443
shice.sc <shice.sc@alibaba-inc.com>
2014-12-12 10:09:52 +0800

    handle datax type

--- oceanbasewriter/src/main/java/com/alibaba/datax/plugin/writer/oceanbasewriter/advance/ast/PredefinedExpression.java
+++ oceanbasewriter/src/main/java/com/alibaba/datax/plugin/writer/oceanbasewriter/advance/ast/PredefinedExpression.java
@@ -1,6 +1,11 @@
 package com.alibaba.datax.plugin.writer.oceanbasewriter.advance.ast;
 
+import com.alibaba.datax.common.element.Column;
 import com.alibaba.datax.common.element.Record;
+import com.google.common.collect.Maps;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 public class PredefinedExpression implements Expression {
 
@@ -17,7 +22,69 @@ public class PredefinedExpression implements Expression {
 					String.format(
                             "%s out of range. Tip: offset from 0 to N-1. source record [%s]",
                             "F" + index, record.toString()));
-		return record.getColumn(index).getRawData();
+		return DataxType.fetchValue(record.getColumn(index));
 	}
 
+    private static enum DataxType{
+        NULL {
+            @Override
+            public Object value(Column column) {
+                return null;
+            }
+        }, LONG {
+            @Override
+            public Object value(Column column) {
+                return column.asBigInteger();
+            }
+        }, DOUBLE {
+            @Override
+            public Object value(Column column) {
+                return column.asBigDecimal();
+            }
+        }, STRING {
+            @Override
+            public Object value(Column column) {
+                return column.asString();
+            }
+        }, BOOL {
+            @Override
+            public Object value(Column column) {
+                return column.asBoolean();
+            }
+        }, DATE {
+            @Override
+            public Object value(Column column) {
+                return column.asDate();
+            }
+        }, BYTES {
+            @Override
+            public Object value(Column column) {
+                try {
+                    return new String(column.asBytes(),"ISO-8859-1");
+                } catch (UnsupportedEncodingException e) {
+                    return null;
+                }
+            }
+        };
+        public abstract Object value(Column column);
+
+        private static final Map<Column.Type,DataxType> types = Maps.newHashMap();
+        static {
+            types.put(Column.Type.NULL, NULL);
+            types.put(Column.Type.LONG, LONG);
+            types.put(Column.Type.DOUBLE, DOUBLE);
+            types.put(Column.Type.STRING, STRING);
+            types.put(Column.Type.BOOL, BOOL);
+            types.put(Column.Type.DATE, DATE);
+            types.put(Column.Type.BYTES, BYTES);
+        }
+
+        public static Object fetchValue(Column column){
+            if (column == null) return null;
+            if(types.containsKey(column.getType())){
+                return types.get(column.getType()).value(column);
+            }
+            throw new UnsupportedOperationException("not support type " + column.toString());
+        }
+    }
 }
