445d8ae9146d92be137b08be8ae551f4436e048c
ZiChi <judy.lt@alibaba-inc.com>
2015-01-30 15:04:11 +0800

    增加odps writer Signed-off-by: ZiChi <judy.lt@alibaba-inc.com>

--- adswriter/src/main/java/com/alibaba/datax/plugin/writer/adswriter/AdsWriter.java
+++ adswriter/src/main/java/com/alibaba/datax/plugin/writer/adswriter/AdsWriter.java
@@ -1,25 +1,42 @@
 package com.alibaba.datax.plugin.writer.adswriter;
 
-import java.util.List;
-
 import com.alibaba.datax.common.plugin.RecordReceiver;
 import com.alibaba.datax.common.spi.Writer;
 import com.alibaba.datax.common.util.Configuration;
+import com.alibaba.datax.plugin.writer.odpswriter.OdpsWriter;
+
+import java.util.List;
 
 
 //TODO writeProxy
 public class AdsWriter extends Writer {
 
+
     public static class Job extends Writer.Job {
+
+        private OdpsWriter.Job odpsWriterProxy = new OdpsWriter.Job();
         private Configuration originalConfig = null;
 
         @Override
         public void init() {
+
+//            this.originalConfig = super.getPluginJobConf();
+//            //创建odps表
+//            Configuration newConf = generate();
+//            super.setPluginConf(newConf);
+
         }
 
         // 一般来说，是需要推迟到 task 中进行pre 的执行（单表情况例外）
         @Override
         public void prepare() {
+            //倒数据到odps表中
+
+            this.odpsWriterProxy.init();
+            this.odpsWriterProxy.prepare();
+            this.odpsWriterProxy.split(3);
+            this.odpsWriterProxy.post();
+            this.odpsWriterProxy.destroy();
         }
 
         @Override
