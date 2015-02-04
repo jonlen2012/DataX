a235b2cfca93f343374c97a67601993862e8a900
ZiChi <judy.lt@alibaba-inc.com>
2015-02-04 14:49:26 +0800

    增加Test Case Signed-off-by: ZiChi <judy.lt@alibaba-inc.com>

--- adswriter/src/test/java/AdsWriteUnitTest.java
+++ adswriter/src/test/java/AdsWriteUnitTest.java
@@ -1,8 +1,16 @@
+import com.alibaba.datax.common.exception.DataXException;
 import com.alibaba.datax.plugin.writer.adswriter.AdsException;
 import com.alibaba.datax.plugin.writer.adswriter.AdsHelper;
+import com.alibaba.datax.plugin.writer.adswriter.AdsWriterErrorCode;
 import com.alibaba.datax.plugin.writer.adswriter.TableMetaHelper;
 import com.alibaba.datax.plugin.writer.adswriter.ads.TableInfo;
 import com.alibaba.datax.plugin.writer.adswriter.odps.TableMeta;
+import com.aliyun.odps.Instance;
+import com.aliyun.odps.Odps;
+import com.aliyun.odps.OdpsException;
+import com.aliyun.odps.account.Account;
+import com.aliyun.odps.account.AliyunAccount;
+import com.aliyun.odps.task.SQLTask;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
@@ -12,6 +20,7 @@ import static org.junit.Assert.assertNull;
  * Created by judy.lt on 2015/2/3.
  */
 public class AdsWriteUnitTest {
+    private final int ODPSOVERTIME = 10000;
     @Test
     public void ads(){
 //        String adsUrl="10.101.90.23:9999";
@@ -20,20 +29,39 @@ public class AdsWriteUnitTest {
         String password="xNXmuBr4dvn3BNLLzWZEAerpHqREto";
         String schema="btest";
         String adsTable="builder_test_table2";
+        String endPoint = "http://service.odpsstg.aliyun-inc.com/stgnew";
+        String accessId = "vw6mmLVuAAa0cHcV";
+        String accessKey = "pG4s6hPmEhglCy9szEEdpBUPTvg0JS";
+        String project = "autotest_dev";
         int lifeCycle = 2;
         AdsHelper adsHelper = new AdsHelper(adsUrl,userName,password,schema);
+        Account odpsAccount = new AliyunAccount(accessId,accessKey);
+        Odps odps = new Odps(odpsAccount);
+        odps.setEndpoint(endPoint);
         try{
             TableInfo tableInfo = adsHelper.getTableInfo(adsTable);
             TableMeta tableMeta = TableMetaHelper.createTempODPSTable(tableInfo, lifeCycle);
             String sql = tableMeta.toDDL();
+            //创建odps表
+            Instance instance = SQLTask.run(odps, project, sql, null, null);
+            String id = instance.getId();
+            boolean terminated = false;
+            int time = 0;
+            while(!terminated && time < ODPSOVERTIME)
+            {
+                Thread.sleep(1000);
+                terminated = instance.isTerminated();
+                time += 1000;
+            }
             System.out.println(sql);
             assertNotNull(sql);
         }catch (AdsException e){
             System.out.println(e);
             assertNull(e);
-
-
-    }
-
+        }catch (OdpsException e) {
+            throw DataXException.asDataXException(AdsWriterErrorCode.ODPS_CREATETABLE_FAILED, e);
+        } catch (InterruptedException e) {
+            throw DataXException.asDataXException(AdsWriterErrorCode.ODPS_CREATETABLE_FAILED,e);
+        }
     }
 }
