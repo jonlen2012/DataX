675bfa80fe4cc0c67f72ade5305688339a615101
shice.sc <shice.sc@alibaba-inc.com>
2015-03-04 16:00:54 +0800

    处理转义

--- oceanbasewriter/src/main/java/com/alibaba/datax/plugin/writer/oceanbasewriter/Rowkey.java
+++ oceanbasewriter/src/main/java/com/alibaba/datax/plugin/writer/oceanbasewriter/Rowkey.java
@@ -3,6 +3,8 @@ package com.alibaba.datax.plugin.writer.oceanbasewriter;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
@@ -13,103 +15,137 @@ import java.util.TreeSet;
 
 public class Rowkey implements Iterable<Rowkey.Entry> {
 
-	private Set<Entry> fields = new TreeSet<Entry>();
+    private Set<Entry> fields = new TreeSet<Entry>();
 
-	private Rowkey(Set<Entry> fields) {
-		this.fields = fields;
-	}
+    private Rowkey(Set<Entry> fields) {
+        this.fields = fields;
+    }
 
-	public static class Entry implements Comparable<Entry> {
+    public static class Entry implements Comparable<Entry> {
 
-		public final String name;
-		public final Type type;
-		public final int position;
+        public final String name;
+        public final Type type;
+        public final int position;
 
-		private Entry(String name, Type type, int position) {
-			this.name = name;
-			this.type = type;
-			this.position = position;
-		}
+        private Entry(String name, Type type, int position) {
+            this.name = name;
+            this.type = type;
+            this.position = position;
+        }
 
-		public enum Type {
+        public enum Type {
 
-			INT, VARCHAR {
-				@Override
-				public String convert(ResultSet result, String column)
-						throws SQLException {
-                    String value = super.convert(result, column);
-					return value == null ? null : "'" + value + "'";
-				}
-			},
-			TIMESTAMP {
-				@Override
-				public String convert(ResultSet result, String column)
-						throws SQLException {
-                    Timestamp timestamp = result.getTimestamp(column);
-					return timestamp == null ? null : String.format("timestamp'%s'",timestamp);
-				}
-			},
-			NUMBER, UNKNOW, BOOL {
-				@Override
-				public String convert(ResultSet result, String column)
-						throws SQLException {
-                    if (result.getObject(column) == null) return null;
-					return String.valueOf(result.getBoolean(column));
-				}
-			};
+            INT {
+                @Override
+                public Long value(ResultSet result, String column) throws SQLException {
+                    return result.getLong(column);
+                }
 
-			public String convert(ResultSet result, String column)
-					throws SQLException {
-				return result.getString(column);
-			}
+                @Override
+                public void convert(int index, PreparedStatement statement, ResultSet result, String column) throws SQLException {
+                    statement.setLong(index, result.getLong(column));
+                }
+            },
+            VARCHAR {
+                @Override
+                public String value(ResultSet result, String column) throws SQLException {
+                    return result.getString(column);
+                }
 
-			private static Map<String, String> DATETIME = ImmutableMap.of(
-					"CREATETIME", "TIMESTAMP", "MODIFYTIME", "TIMESTAMP");
+                @Override
+                public void convert(int index, PreparedStatement statement, ResultSet result, String column)
+                        throws SQLException {
+                    statement.setString(index, result.getString(column));
+                }
+            },
+            TIMESTAMP {
+                @Override
+                public Timestamp value(ResultSet result, String column) throws SQLException {
+                    return result.getTimestamp(column);
+                }
 
-			public static Type type(String label) {
-				label = label.toUpperCase();
-				if (DATETIME.containsKey(label))
-					label = DATETIME.get(label);
-				for (Type type : Type.values()) {
-					if (label.toUpperCase().contains(type.name()))
-						return type;
-				}
-				return UNKNOW;
-			}
-		}
+                @Override
+                public void convert(int index, PreparedStatement statement, ResultSet result, String column)
+                        throws SQLException {
+                    statement.setTimestamp(index, result.getTimestamp(column));
+                }
+            },
+            NUMERIC {
+                @Override
+                public BigDecimal value(ResultSet result, String column) throws SQLException {
+                    return result.getBigDecimal(column);
+                }
 
-		@Override
-		public int compareTo(Entry o) {
-			return this.position - o.position;
-		}
-	}
+                @Override
+                public void convert(int index, PreparedStatement statement, ResultSet result, String column) throws SQLException {
+                    statement.setBigDecimal(index, result.getBigDecimal(column));
+                }
+            },
+            BOOL {
+                @Override
+                public Boolean value(ResultSet result, String column) throws SQLException {
+                    return result.getBoolean(column);
+                }
 
-	@Override
-	public Iterator<Entry> iterator() {
-		return fields.iterator();
-	}
+                @Override
+                public void convert(int index, PreparedStatement statement, ResultSet result, String column)
+                        throws SQLException {
+                    statement.setBoolean(index, result.getBoolean(column));
+                }
+            };
 
-	public static Builder builder() {
-		return new Builder();
-	}
+            public abstract <T> T value(ResultSet result, String column) throws SQLException;
+            public abstract void convert(int index, PreparedStatement statement, ResultSet result, String column) throws SQLException;
 
-	public static final class Builder {
+            private static Map<String, String> DATETIME = ImmutableMap.of(
+                    "CREATETIME", "TIMESTAMP", "MODIFYTIME", "TIMESTAMP");
 
-		private Builder() {
-		};
+            public static Type type(String label) {
+                label = label.toUpperCase();
+                if (DATETIME.containsKey(label))
+                    label = DATETIME.get(label);
+                for (Type type : Type.values()) {
+                    if (label.toUpperCase().contains(type.name()))
+                        return type;
+                }
+                throw new IllegalStateException("OB return a plugin not support type " + label);
+            }
+        }
 
-		private boolean hasBuild = false;
-		private Set<Entry> fields = new TreeSet<Entry>();
+        @Override
+        public int compareTo(Entry o) {
+            return this.position - o.position;
+        }
+    }
 
-		public Builder addEntry(String name, String type, int position) {
-			Preconditions.checkState(!hasBuild,"can't call addEntry() once build() has called");
-			fields.add(new Entry(name, Entry.Type.type(type), position));
-			return this;
-		}
+    @Override
+    public Iterator<Entry> iterator() {
+        return fields.iterator();
+    }
 
-		public Rowkey build() {
-			Preconditions.checkState(!fields.isEmpty(),"first call addEntry() at least once");
-			return new Rowkey(this.fields);
-		}
-	}
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+
+        private Builder() {
+        }
+
+        ;
+
+        private boolean hasBuild = false;
+        private Set<Entry> fields = new TreeSet<Entry>();
+
+        public Builder addEntry(String name, String type, int position) {
+            Preconditions.checkState(!hasBuild, "can't call addEntry() once build() has called");
+            fields.add(new Entry(name, Entry.Type.type(type), position));
+            return this;
+        }
+
+        public Rowkey build() {
+            Preconditions.checkState(!fields.isEmpty(), "first call addEntry() at least once");
+            return new Rowkey(this.fields);
+        }
+    }
 }
