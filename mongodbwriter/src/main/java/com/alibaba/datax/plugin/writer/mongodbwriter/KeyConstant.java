e5eecd1dbcbf9f6ee29a73d07c6ef2b27ffbbfb5
jianying.wcj <jianying.wcj@taobao.com>
2015-03-22 21:02:42 +0800

    mongo db sync plugin

--- mongodbwriter/src/main/java/com/alibaba/datax/plugin/writer/mongodbwriter/KeyConstant.java
+++ mongodbwriter/src/main/java/com/alibaba/datax/plugin/writer/mongodbwriter/KeyConstant.java
@@ -55,5 +55,5 @@ public class KeyConstant {
     /**
      * 指定用来判断是否覆盖的 业务主键
      */
-    public static final String UNIQUE_KEY = "unique_key";
+    public static final String UNIQUE_KEY = "unique_id";
 }
