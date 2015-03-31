c075c5c3f4d2fed017f6505e8f95eb4745f7a0aa
tianjin.lp <tianjin.lp@taobao.com>
2015-03-31 19:15:54 +0800

    修改tddl的与标准的java.sql的type的对应关系

--- tddlwriter/src/main/java/com/alibaba/datax/core/writer/tddlwriter/TddlCommonRdbmsWriter.java
+++ tddlwriter/src/main/java/com/alibaba/datax/core/writer/tddlwriter/TddlCommonRdbmsWriter.java
@@ -1,12 +1,12 @@
 package com.alibaba.datax.core.writer.tddlwriter;
 
 import com.alibaba.datax.common.element.Record;
-import com.alibaba.datax.common.exception.DataXException;
 import com.alibaba.datax.common.util.Configuration;
 import com.alibaba.datax.plugin.rdbms.util.DataBaseType;
 import com.alibaba.datax.plugin.rdbms.writer.CommonRdbmsWriter;
 import com.alibaba.datax.plugin.rdbms.writer.Constant;
 import com.alibaba.datax.plugin.rdbms.writer.Key;
+import com.taobao.tddl.optimizer.core.datatype.DataType;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -47,6 +47,7 @@ public class TddlCommonRdbmsWriter extends CommonRdbmsWriter {
                     case Types.INTEGER:
                     case Types.SMALLINT:
                     case Types.BIGINT:
+                    case DataType.MEDIAUMNINT_SQL_TYPE :
                         preparedStatement.setLong(i + 1, record.getColumn(i).asLong());
                         break;
                     case Types.NUMERIC:
@@ -56,21 +57,11 @@ public class TddlCommonRdbmsWriter extends CommonRdbmsWriter {
                     case Types.DOUBLE:
                         preparedStatement.setBigDecimal(i + 1, record.getColumn(i).asBigDecimal());
                         break;
-                    //TODO 5.1.18版本会走此流程
-                    case 10003:
-                        java.sql.Timestamp sqlTimestamp = null;
-                        java.util.Date utilDate;
-                        try {
-                            utilDate = record.getColumn(i).asDate();
-                        } catch (DataXException e) {
-                            throw new SQLException(String.format(
-                                    "TIMESTAMP 类型转换错误：[%s]", record.getColumn(i)));
-                        }
-
-                        if (null != utilDate) {
-                            sqlTimestamp = new java.sql.Timestamp(utilDate.getTime());
-                        }
-                        preparedStatement.setTimestamp(i + 1, sqlTimestamp);
+                    case DataType.DATETIME_SQL_TYPE:
+                        preparedStatement = super.fillPreparedStatementColumnType(preparedStatement, i, Types.TIMESTAMP, record.getColumn(i));
+                        break;
+                    case DataType.YEAR_SQL_TYPE:
+                        preparedStatement = super.fillPreparedStatementColumnType(preparedStatement, i, Types.DATE, record.getColumn(i));
                         break;
                     default:
                         preparedStatement = super.fillPreparedStatementColumnType(preparedStatement, i, columnSqltype, record.getColumn(i));
