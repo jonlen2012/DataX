b7ec8247b0411a11818ce4ff32525818b62bdb81
tianjin.lp <tianjin.lp@taobao.com>
2015-04-03 13:15:33 +0800

    datax_autocommit_bug_fix

--- plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/writer/CommonRdbmsWriter.java
+++ plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/writer/CommonRdbmsWriter.java
@@ -254,11 +254,6 @@ public class CommonRdbmsWriter {
                     this.jdbcUrl, username, password);
             DBUtil.dealWithSessionConfig(connection, writerSliceConfig,
                     this.dataBaseType, BASIC_MESSAGE);
-            try {
-                connection.setAutoCommit(false);
-            } catch (Exception e) {
-                throw DataXException.asDataXException(DBUtilErrorCode.WRITE_DATA_ERROR, e);
-            }
             startWriteWithConnection(recordReceiver, taskPluginCollector, connection);
         }
 
@@ -288,6 +283,7 @@ public class CommonRdbmsWriter {
                 throws SQLException {
             PreparedStatement preparedStatement = null;
             try {
+                connection.setAutoCommit(false);
                 preparedStatement = connection
                         .prepareStatement(this.writeRecordSql);
 
