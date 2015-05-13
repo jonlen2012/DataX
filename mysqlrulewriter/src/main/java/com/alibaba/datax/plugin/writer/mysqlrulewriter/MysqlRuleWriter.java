a53225fd0ad7636803641d633ec1377f073e17b0
tianjin.lp <tianjin.lp@taobao.com>
2015-05-13 19:47:27 +0800

    修改writer包名称

--- mysqlrulewriter/src/main/java/com/alibaba/datax/plugin/writer/mysqlrulewriter/MysqlRuleWriter.java
+++ mysqlrulewriter/src/main/java/com/alibaba/datax/plugin/writer/mysqlrulewriter/MysqlRuleWriter.java
@@ -1,11 +1,9 @@
 package com.alibaba.datax.plugin.writer.mysqlrulewriter;
 
-import com.alibaba.datax.common.exception.DataXException;
 import com.alibaba.datax.common.plugin.RecordReceiver;
 import com.alibaba.datax.common.spi.Writer;
 import com.alibaba.datax.common.util.Configuration;
 import com.alibaba.datax.plugin.rdbms.util.DBUtil;
-import com.alibaba.datax.plugin.rdbms.util.DBUtilErrorCode;
 import com.alibaba.datax.plugin.rdbms.util.DataBaseType;
 import com.alibaba.datax.plugin.rdbms.writer.CommonRdbmsWriter;
 import com.alibaba.datax.plugin.rdbms.writer.Constant;
@@ -33,11 +31,6 @@ public class MysqlRuleWriter extends Writer {
 
             this.commonRdbmsWriterJob = new CommonRdbmsWriter.Job(DATABASE_TYPE);
             this.commonRdbmsWriterJob.init(this.originalConfig);
-            // 检查 db/table规则 配置（必填）
-            originalConfig.getNecessaryValue(Key.DB_NAME_PATTERN, DBUtilErrorCode.REQUIRED_VALUE);
-            originalConfig.getNecessaryValue(Key.DB_RULE, DBUtilErrorCode.REQUIRED_VALUE);
-            originalConfig.getNecessaryValue(Key.TABLE_NAME_PATTERN, DBUtilErrorCode.REQUIRED_VALUE);
-            originalConfig.getNecessaryValue(Key.TABLE_RULE, DBUtilErrorCode.REQUIRED_VALUE);
         }
 
         // 一般来说，是需要推迟到 task 中进行pre 的执行（单表情况例外）
@@ -47,17 +40,17 @@ public class MysqlRuleWriter extends Writer {
 
             int tableNumber = originalConfig.getInt(Constant.TABLE_NUMBER_MARK);
             if (tableNumber == 1) {
-                throw DataXException.asDataXException(DBUtilErrorCode.CONF_ERROR, "tableNumber=1, mysqlrulewriter只能支持分库分表任务");
+                throw new RuntimeException("mysqlrulewriter只能支持分库分表任务");
             }
             String username = originalConfig.getString(Key.USERNAME);
             String password = originalConfig.getString(Key.PASSWORD);
 
-            //获取presql配置，并执行
             List<String> preSqls = originalConfig.getList(Key.PRE_SQL, String.class);
+            // 此处删除掉PRE SQL配置
             originalConfig.remove(Key.PRE_SQL);
 
-            //TODO String.class
-            List<Object> conns = originalConfig.getList(Constant.CONN_MARK, Object.class);
+            List<Object> conns = originalConfig.getList(Constant.CONN_MARK,
+                    Object.class);
             for(Object connConfObject : conns) {
                 Configuration connConf = Configuration.from(connConfObject.toString());
                 // 这里的 jdbcUrl 已经 append 了合适后缀参数
@@ -67,7 +60,8 @@ public class MysqlRuleWriter extends Writer {
                 for (String table : tableList) {
                     List<String> renderedPreSqls = WriterUtil.renderPreOrPostSqls(preSqls, table);
                     if (null != renderedPreSqls && !renderedPreSqls.isEmpty()) {
-                        Connection conn = DBUtil.getConnection(DATABASE_TYPE, jdbcUrl, username, password);
+                        Connection conn = DBUtil.getConnection(DATABASE_TYPE,
+                                jdbcUrl, username, password);
                         LOG.info("Begin to execute preSqls:[{}]. context info:{}.",
                                 StringUtils.join(renderedPreSqls, ";"), jdbcUrl);
 
@@ -76,11 +70,10 @@ public class MysqlRuleWriter extends Writer {
                     }
                 }
             }
-            //TODO 减少计算
-            LOG.debug("After job prepare(), originalConfig now is:[\n{}\n]", originalConfig.toJSON());
+            LOG.debug("After job prepare(), originalConfig now is:[\n{}\n]",
+                    originalConfig.toJSON());
         }
 
-        //TODO copy风险 一个tg一个配置
         @Override
         public List<Configuration> split(int mandatoryNumber) {
             Configuration simplifiedConf = this.originalConfig;
