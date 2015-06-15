3bfaee2a40f7bd3378e0da2b30a35ddc64765d12
ZiChi <judy.lt@alibaba-inc.com>
2015-06-15 13:33:50 +0800

    Signed-off-by: ZiChi <judy.lt@alibaba-inc.com>

--- plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/reader/CommonRdbmsReader.java
+++ plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/reader/CommonRdbmsReader.java
@@ -20,6 +20,7 @@ import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
