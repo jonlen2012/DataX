f7a0596891a92b0bb9b5a195e12f95b39fe41a94
骑风 <qifeng.sxm@alibaba-inc.com>
2015-12-17 16:55:26 +0800

    修改hbase写入插件 修改配置项 修改脏数据收集

--- hbasewriter/src/main/java/com/alibaba/datax/plugin/writer/hbasewriter/Const.java
+++ hbasewriter/src/main/java/com/alibaba/datax/plugin/writer/hbasewriter/Const.java
@@ -4,24 +4,10 @@ package com.alibaba.datax.plugin.writer.hbasewriter;
  * Created by qifeng.sxm on 2015/12/11.
  */
 public class Const {
-	
-    public static final String ZOOKEEPER_SERVER_HOSTS = "zips";
-
-
-    public static final String ZOOKEEPER_CLIENT_PORT = "zport";
-
-
-    public static final String TABLE_NAME = "table";
-
-
-    public static final String COLUMN_FAMILY = "colf";
-
-    
-    public static final String COLUMN_NAMES = "columns";
-    
-    
-    public static final String BATCH_ROWS = "batchrows";
-    
-    
-    public static final String DEFAULT_COLUMN_FAMILY = "cf";
+	public static final String DEFAULT_NULLMODE = "EMPTY_BYTES";
+	public static final String DEFAULT_ENCODING = "utf-8";
+	public static final int DEFAULT_BATCHROWS = 100;
+	public static final int MAX_BATCHROWS = 1000;
+	public static final String DEFAULT_COLUMN_FAMILY = "cf";
+	public static final String DEFAULT_QUALIFIER = "unknown";
 }
--- hbasewriter/src/main/java/com/alibaba/datax/plugin/writer/hbasewriter/HBaseWriterErrorCode.java
+++ hbasewriter/src/main/java/com/alibaba/datax/plugin/writer/hbasewriter/HBaseWriterErrorCode.java
@@ -6,13 +6,21 @@ import com.alibaba.datax.common.spi.ErrorCode;
  * Created by qifeng.sxm on 2015/12/11.
  */
 public enum HBaseWriterErrorCode implements ErrorCode {
-
-    ZIPS_CONFIG_ERROR("HBaseWriter-01", "没有配置 zips"),
-    ZPORT_CONFIG_ERROR("HBaseWriter-02", "没有配置 zport"),
-    TABLE_NAME_CONFIG_ERROR("HBaseWriter-03", "没有配置 table_name"),
-    COLUMN_FAMILY_CONFIG_ERROR("HBaseWriter-04", "没有配置 column_family"),
-    COLUMN_NAMES_CONFIG_ERROR("HBaseWriter-05", "没有配置 column_names"),
-    ILLEGAL_VALUES_ERROR("HBaseWriter-06", "字段个数不一致");
+	
+	HBASE_TABLE_CONF_ERROR("HBaseWriter-01","HBase表名没有配置"),
+	HBASE_ROWKEY_CONF_ERROR("HBaseWriter-02","HBase表主键没有配置或配置不正确,格式:0|string"),
+	HBASE_COLUMN_CONF_ERROR("HBaseWriter-03","HBase表字段没有配置或配置不正确,格式:1|string|cf:request_time,2|string|cf:host,3|string|cf:uri"),
+	HBASE_NULLMODE_CONF_ERROR("HBaseWriter-04","HBase表数据空值默认值没有配置"),
+	HBASE_ENCODE_CONF_ERROR("HBaseWriter-05","HBase表数据编码没有配置"),
+	HBASE_BATCHROWS_CONF_ERROR("HBaseWriter-06","HBase表数据导入批量单位没有配置"),
+	HBASE_CONF_ERROR("HBaseWriter-07","HBase客户端configuration没有配置或配置不正确"),
+	
+	ZK_PARENT_CONF_ERROR("HBaseWriter-08","HBase在ZooKeeper中的根目录没有配置或配置不正确,格式:/hbase"),
+	ZK_QUORUM_CONF_ERROR("HBaseWriter-09","ZooKeeper集群可服务主机列表没有配置或配置不正确,格式:ip1,ip2,ip3"),
+	ZK_CLIENTPORT_CONF_ERROR("HBaseWriter-10","ZooKeeper集群客户端连接端口没有配置或配置不正确"),
+    ILLEGAL_VALUES_ERROR("HBaseWriter-11", "配置字段个数出现不一致"),
+    
+    HBASE_RUNNING_ERROR("HBaseWriter-12", "HBase客户端运行时异常");
 
     HBaseWriterErrorCode(String code, String desc) {
         this.code = code;
